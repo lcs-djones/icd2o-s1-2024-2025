50d54886be8104ddc54abb662cffe329382cb4a6
Russell Gordon <rgordon@rgordon-LCS-Air.home>
2024-09-09 13:27:09 -0400

    Fix section number in home page link

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "📚 ICD2O S2",
+    pageTitle: "📚 ICD2O S1",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
